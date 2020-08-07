8f18e98ca907206b0f1ffda24e2acc91aa776d9b
BlankGodd <blankgodd33@gmail.com>
2020-08-07 01:37:13 +0100

    Dark bar

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # Author:   @BlankGodd_
 
-from colpy.loadbar import Load_bar
+from colpy.loadbar import *
 
-loadbar = Load_bar()
+loadbar = Dark_bar()
 
 if __name__ == '__main__':
     loadbar.iter(iterator=10000000)
